0d47fd74cf9bed905bfe852ff76449a2e0d6a9bd
nhtmai <mainhatng@gmail.com>
2025-02-18 22:13:10 -0500

    attempt #2

--- a1q2.c
+++ a1q2.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
     
 #define BUFSIZE 256
     
@@ -11,13 +10,29 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-
-    int CMD_SIZE = 8;
-    int FILENAME_SIZE = strlen(argv[1]);
+    FILE* fp = fopen(argv[1], "r"); 
+  
+    // checking if the file exists and can be read
+    if (fp == NULL) { 
+        fprintf(stderr, "File Not Found!\n"); 
+        return -1; 
+    } 
     
-    if (CMD_SIZE + FILENAME_SIZE < BUFSIZE) {
-        strncat(cmd, argv[1], FILENAME_SIZE);
-        system(cmd);
+    int seek_status = fseek(fp, 0, SEEK_END);
+    
+    if (seek_status != 0) { // moving to end of file failed
+        fclose(fp);
+        fprintf(stderr, "Cannot determine file size!\n");
+        return -1;
+    }
+
+    // getting size of file 
+    long int file_size = ftell(fp);
+    fclose(fp);
+    if (file_size == -1L) { // if failed
+        fprintf(stderr, "Cannot determine file size!\n");
+        return -1;
+    } else {
+        printf("%ld\n", file_size);
     }
 }
