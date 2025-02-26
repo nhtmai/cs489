87508c0fad63b87a36300bef8ff896321274509b
nhtmai <mainhatng@gmail.com>
2025-02-26 16:50:35 -0500

    upload A2 code

--- a1q2.c
+++ a1q2.c
@@ -1,36 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
     
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
-    }
-
-    FILE* fp = fopen(argv[1], "r");  // opening file
-    if (fp == NULL) { // if opening file failed
-        fprintf(stderr, "File Not Found!\n"); 
-        return -1; 
-    } 
-    
-    int seek_status = fseek(fp, 0, SEEK_END);
-    if (seek_status != 0) { // if moving to end of file failed
-        fclose(fp);
-        fprintf(stderr, "Cannot determine file size!\n");
-        return -1;
-    }
-
-    // getting size of file 
-    long int file_size = ftell(fp);
-    fclose(fp); // closing file
-    if (file_size == -1L) { // if getting size failed
-        fprintf(stderr, "Cannot determine file size!\n");
-        return -1;
-    } else {
-        printf("%ld\n", file_size);
-    }
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index-1]);
 }
